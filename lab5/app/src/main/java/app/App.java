8c1e52f75bdf60fcaa74e8304786b83d268f7cbf
AntonSexov <antonvedernikov2003@gmail.com>
2023-02-22 23:02:32 +0300

    almost finished person creator :)

--- lab5/app/src/main/java/app/App.java
+++ lab5/app/src/main/java/app/App.java
@@ -3,19 +3,19 @@ import app.data.*;
 import app.utils.*;
 import app.commands.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 public class App {
     public String getGreeting(){
         return "hello";
     }
     public static void main(String[] args) {
+        PersonCreator pc = new PersonCreator();
         CollectionHandler cHandler = new CollectionHandler();
-        Person p = new Person(cHandler.generateNextId(), "oleg", new Coordinates(0, 0), LocalDate.now(), 182.0f, LocalDateTime.now(), Color.GREEN, Color.BROWN, null);
+        Person p = new Person(cHandler.generateNextId(), pc.nameCreate(), pc.coordinatesCreate(),
+         LocalDate.now(),pc.heightCreate(), pc.bdayCreate(), Color.GREEN, Color.BROWN, pc.locationCreate());
         cHandler.addPerson(p);
         cHandler.printPersonList();
         AbstractCommand info = new Info(cHandler);
         info.execute("");
     }
-    //TODO: CollectionHandler - done
 }
--- lab5/app/src/main/java/app/utils/PersonCreator.java
+++ lab5/app/src/main/java/app/utils/PersonCreator.java
@@ -1,27 +1,108 @@
 package app.utils;
-import app.utils.*;
-import app.data.*;
+import app.data.Coordinates;
+import app.data.Location;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class PersonCreator {
     Scanner scanner;
-
     public PersonCreator(){
         scanner = new Scanner(System.in);
     }
 
     public String nameCreate(){
-        String name = "";
+        String name = null;
         try{
             IOHandler.println("Введите имя: ");
             name = scanner.nextLine();
+            
         }catch (InputMismatchException e){
             IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
             nameCreate();
         }
         return name;
+    }
+
+    public Coordinates coordinatesCreate(){
+        int x = 0;
+        long y = 0;
+        try{
+            IOHandler.println("Координаты:\nВведите долготу: ");
+            x = scanner.nextInt();
+            IOHandler.println("Введите широту: ");
+            y = scanner.nextLong();
+        }catch (InputMismatchException e){
+            IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
+            coordinatesCreate();
+        }
+        return new Coordinates(x, y); 
+    }
+
+    public Float heightCreate(){
+        Float height = null;
+        try{
+            IOHandler.println("Введите рост: ");
+            height = scanner.nextFloat();
+        }catch (InputMismatchException e){
+            IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
+            heightCreate();
+        }
+        return height; 
+    }
+
+    public LocalDateTime bdayCreate(){
+        DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        String inputTime = "";
+        LocalDateTime time = null; 
+        try{
+            scanner.nextLine();
+            IOHandler.println("Введите дату и время рождения(год-месяц-день час:минуты): ");
+            inputTime = scanner.nextLine();
+            time = LocalDateTime.parse(inputTime, formatter);
+        }catch (InputMismatchException e){
+            IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
+            bdayCreate();
+        }
+        return time; 
+    }
+
+    // public LocalDateTime eyeColorChoose(){
+    //     DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    //     String inputTime = "";
+    //     LocalDateTime time = null; 
+    //     try{
+    //         IOHandler.println("Введите дату и время рождения(\"год-месяц-день час:минуты\"): ");
+    //         inputTime = scanner.nextLine();
+    //         time = LocalDateTime.parse(inputTime, formatter);
+    //     }catch (InputMismatchException e){
+    //         IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
+    //         eyeColorChoose();
+    //     }
+    //     return color; 
+    // }
+
+    public Location locationCreate(){
+        Integer x = 0;
+        Double y = 0D;
+        Double z = 0D;
+        String name = " ";
+        try{
+            IOHandler.println("Введите координаты и название местоположения:\n Введите долготу: ");
+            x = scanner.nextInt();
+            IOHandler.println("Введите широту: ");
+            y = scanner.nextDouble();
+            IOHandler.println("Введите высоту: ");
+            z = scanner.nextDouble();
+            IOHandler.println("Введите долготу: ");
+            name = scanner.nextLine();
+        }catch (InputMismatchException e){
+            IOHandler.println("Неправильный формат введенных данных, попробуйте еще раз.");
+            locationCreate();
+        }
+        return new Location(x, y, z, name); 
     }
     //WIP
 }
