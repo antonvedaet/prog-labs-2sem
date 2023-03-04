1ff9101b1e308dd0e4c84cae17a5eb6f45bf3a36
AntonSexov <antonvedernikov2003@gmail.com>
2023-03-04 06:23:37 +0300

    javadoc

---
+++ lab5/app/build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"app"},{"l":"app.commands"},{"l":"app.data"},{"l":"app.exceptions"},{"l":"app.utils"}];updateSearchResults();
