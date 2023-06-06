a03b4e75494c954c3f4645733752fb202381b484
Антон Ведерников <82718776+AntonSexov@users.noreply.github.com>
2023-06-06 04:03:20 +0300

    auth window

--- lab8/app/src/main/java/ifmo/gui/controllers/AuthController.java
+++ lab8/app/src/main/java/ifmo/gui/controllers/AuthController.java
@@ -5,6 +5,7 @@ import java.net.URL;
 import ifmo.data.User;
 import ifmo.network.TCPClient;
 import ifmo.utils.UserHelper;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,13 +13,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class AuthController {
     @FXML private Button loginButton;
     @FXML private Button registerButton;
     @FXML private TextField loginField;
     @FXML private PasswordField passwordField;
+    @FXML private StackPane popupPane;
     private TCPClient _tcpClient;
     private UserHelper userHelper;
 
@@ -29,6 +33,31 @@ public class AuthController {
     }
 
     @FXML
+    public void showErrorPopup() {
+        popupPane.setVisible(true);
+        PauseTransition visiblePause = new PauseTransition(Duration.seconds(3));
+        visiblePause.setOnFinished(event -> popupPane.setVisible(false));
+        visiblePause.play();
+    }
+
+
+    @FXML
+    public void showTable(ActionEvent event){
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Table.fxml"));
+        loader.setController(new TableController(_tcpClient));
+        Parent root;
+        try {
+            root = loader.load();
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene nextScene = new Scene(root);
+            primaryStage.setScene(nextScene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
     public void handleButton(ActionEvent event) {
 
         String login = loginField.getText();
@@ -36,24 +65,17 @@ public class AuthController {
         User user = new User(login, password);
 
         if (event.getSource() == loginButton) {
-            // if(userHelper.verify(userHelper.sendRegister(user), user)){
-            //     //transfer to next page
-            // }
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Table.fxml"));
-            loader.setController(new TableController(_tcpClient));
-            Parent root;
-            try {
-                root = loader.load();
-                Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                Scene nextScene = new Scene(root);
-                primaryStage.setScene(nextScene);
-                primaryStage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
+            if(userHelper.verify(userHelper.sendLogin(user), user)){
+                showTable(event);
+            } else {
+                showErrorPopup();
             }
-        } else if (event.getSource() == registerButton) {
+        } 
+        if (event.getSource() == registerButton) {
             if(userHelper.verify(userHelper.sendRegister(user), user)){
-                //transfer to next page
+                showTable(event);
+            } else {
+                showErrorPopup();
             }
         }
     }
